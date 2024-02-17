6d2a85217de704b69c3f9656f69863c38af706cd
solwanmahmoud <117037484+solwanmahmoud@users.noreply.github.com>
2024-02-17 14:38:28 +0200

    category first step

---
+++ BookStore.User/Forms/BookControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookStore.User.Forms
+{
+  public partial class BookControl : UserControl
+  {
+    public string BookName { get => label1.Text; set => label1.Text = value; }
+    public string BookPrice { get => label2.Text; set => label2.Text = value; }
+
+    public Image BookImage { get => pictureBox1?.BackgroundImage; set => pictureBox1.BackgroundImage = value; }
+
+    public BookControl()
+    {
+      InitializeComponent();
+      pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+      Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+      button1.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, button1.Width, button1.Height, 15, 15));
+    }
+
+    [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+    private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+  }
+}
---
+++ BookStore.User/Forms/BooksForm.cs
@@ -0,0 +1,71 @@
+﻿using BookStore.User.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookStore.User.Forms
+{
+  public partial class BooksForm : Form
+  {
+    List<BookControl> books = new();
+
+    public BooksForm()
+    {
+      InitializeComponent();
+      Random random = new Random();
+      for (int i = 0; i < 10; i++)
+      {
+        var book = new BookControl();
+        book.BookName = "BookTitle";
+        book.BookPrice = "Price $";
+        book.BookImage = Image.FromFile(Path.GetFullPath($"..\\..\\..\\Images\\Book{random.Next(16)+1}.jpg"));
+        //Bitmap b = new Bitmap(book.BookImage);
+        //book.BookImage = ResizeImage(b, new Size(600, 450));
+        books.Add(book);
+      }
+      foreach (var book in books)
+        flowLayoutPanel1.Controls.Add(book);
+
+    }
+
+    private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+    {
+
+    }
+
+    //private static Image ResizeImage(Image imgToResize, Size size)
+    //{
+    //  // Get the image current width
+    //  int sourceWidth = imgToResize.Width;
+    //  // Get the image current height
+    //  int sourceHeight = imgToResize.Height;
+    //  float nPercent = 0;
+    //  float nPercentW = 0;
+    //  float nPercentH = 0;
+    //  // Calculate width and height with new desired size
+    //  nPercentW = ((float)size.Width / (float)sourceWidth);
+    //  nPercentH = ((float)size.Height / (float)sourceHeight);
+    //  nPercent = Math.Min(nPercentW, nPercentH);
+    //  // New Width and Height
+    //  int destWidth = (int)(sourceWidth * nPercent);
+    //  int destHeight = (int)(sourceHeight * nPercent);
+    //  Bitmap b = new Bitmap(destWidth, destHeight);
+    //  Graphics g = Graphics.FromImage((Image)b);
+    //  g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+    //  // Draw image with new width and height
+    //  g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+    //  g.Dispose();
+    //  return (Image)b;
+    //}
+
+  }
+
+
+}
