3688afbcecc7dc24f9b93b2af8d795125ceff5a2
solwanmahmoud <117037484+solwanmahmoud@users.noreply.github.com>
2024-02-17 17:21:29 +0200

    last ui category form modification

--- BookStore.User/Forms/CategoriesForm.Designer.cs
+++ BookStore.User/Forms/CategoriesForm.Designer.cs
@@ -34,7 +34,7 @@
             pictureBox4 = new PictureBox();
             pictureBox5 = new PictureBox();
             label1 = new Label();
-            button2 = new Button();
+            button6 = new Button();
             button1 = new Button();
             button3 = new Button();
             button4 = new Button();
@@ -49,9 +49,9 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.design_thinking;
-            pictureBox1.Location = new Point(87, 153);
+            pictureBox1.Location = new Point(99, 279);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(107, 116);
+            pictureBox1.Size = new Size(233, 239);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -59,9 +59,9 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.poster;
-            pictureBox2.Location = new Point(347, 153);
+            pictureBox2.Location = new Point(676, 279);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(107, 116);
+            pictureBox2.Size = new Size(233, 239);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
@@ -69,9 +69,9 @@
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.pencil_case;
-            pictureBox3.Location = new Point(473, 153);
+            pictureBox3.Location = new Point(963, 279);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(107, 116);
+            pictureBox3.Size = new Size(233, 239);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 2;
             pictureBox3.TabStop = false;
@@ -79,9 +79,9 @@
             // pictureBox4
             // 
             pictureBox4.Image = Properties.Resources.science_book;
-            pictureBox4.Location = new Point(601, 153);
+            pictureBox4.Location = new Point(1254, 279);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(107, 116);
+            pictureBox4.Size = new Size(233, 239);
             pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox4.TabIndex = 3;
             pictureBox4.TabStop = false;
@@ -90,9 +90,9 @@
             // 
             pictureBox5.BackColor = SystemColors.Control;
             pictureBox5.Image = Properties.Resources.globe;
-            pictureBox5.Location = new Point(214, 153);
+            pictureBox5.Location = new Point(390, 279);
             pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(107, 116);
+            pictureBox5.Size = new Size(235, 239);
             pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox5.TabIndex = 4;
             pictureBox5.TabStop = false;
@@ -100,90 +100,100 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Century Gothic", 22F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Century Gothic", 28F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.FromArgb(255, 128, 128);
-            label1.Location = new Point(214, 60);
+            label1.Location = new Point(511, 129);
             label1.Name = "label1";
-            label1.Size = new Size(362, 51);
+            label1.Size = new Size(462, 66);
             label1.TabIndex = 5;
             label1.Text = "Books Category";
             label1.Click += label1_Click;
             // 
-            // button2
+            // button6
             // 
-            button2.BackColor = Color.WhiteSmoke;
-            button2.Cursor = Cursors.Hand;
-            button2.Font = new Font("Century Gothic", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.ForeColor = Color.FromArgb(39, 55, 77);
-            button2.Location = new Point(87, 275);
-            button2.Name = "button2";
-            button2.Size = new Size(107, 32);
-            button2.TabIndex = 15;
-            button2.Text = "Code";
-            button2.UseVisualStyleBackColor = false;
+            button6.BackColor = Color.FromArgb(39, 55, 77);
+            button6.Cursor = Cursors.Hand;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            button6.ForeColor = Color.FromArgb(255, 192, 192);
+            button6.Location = new Point(99, 533);
+            button6.Name = "button6";
+            button6.Size = new Size(233, 57);
+            button6.TabIndex = 20;
+            button6.Text = "Code";
+            button6.UseVisualStyleBackColor = false;
             // 
             // button1
             // 
-            button1.BackColor = Color.WhiteSmoke;
+            button1.BackColor = Color.FromArgb(39, 55, 77);
             button1.Cursor = Cursors.Hand;
-            button1.Font = new Font("Century Gothic", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.ForeColor = Color.FromArgb(39, 55, 77);
-            button1.Location = new Point(347, 275);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            button1.ForeColor = Color.FromArgb(255, 192, 192);
+            button1.Location = new Point(392, 533);
             button1.Name = "button1";
-            button1.Size = new Size(107, 32);
-            button1.TabIndex = 16;
-            button1.Text = "Suspense";
+            button1.Size = new Size(233, 57);
+            button1.TabIndex = 25;
+            button1.Text = "Fiction";
             button1.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            button3.BackColor = Color.WhiteSmoke;
+            button3.BackColor = Color.FromArgb(39, 55, 77);
             button3.Cursor = Cursors.Hand;
-            button3.Font = new Font("Century Gothic", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button3.ForeColor = Color.FromArgb(39, 55, 77);
-            button3.Location = new Point(601, 275);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            button3.ForeColor = Color.FromArgb(255, 192, 192);
+            button3.Location = new Point(1254, 533);
             button3.Name = "button3";
-            button3.Size = new Size(107, 32);
-            button3.TabIndex = 17;
+            button3.Size = new Size(233, 57);
+            button3.TabIndex = 26;
             button3.Text = "Science";
             button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
-            button4.BackColor = Color.WhiteSmoke;
+            button4.BackColor = Color.FromArgb(39, 55, 77);
             button4.Cursor = Cursors.Hand;
-            button4.Font = new Font("Century Gothic", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button4.ForeColor = Color.FromArgb(39, 55, 77);
-            button4.Location = new Point(473, 275);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            button4.ForeColor = Color.FromArgb(255, 192, 192);
+            button4.Location = new Point(963, 533);
             button4.Name = "button4";
-            button4.Size = new Size(107, 32);
-            button4.TabIndex = 18;
+            button4.Size = new Size(233, 57);
+            button4.TabIndex = 27;
             button4.Text = "Beauty";
             button4.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
-            button5.BackColor = Color.WhiteSmoke;
+            button5.BackColor = Color.FromArgb(39, 55, 77);
             button5.Cursor = Cursors.Hand;
-            button5.Font = new Font("Century Gothic", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button5.ForeColor = Color.FromArgb(39, 55, 77);
-            button5.Location = new Point(214, 275);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            button5.ForeColor = Color.FromArgb(255, 192, 192);
+            button5.Location = new Point(676, 533);
             button5.Name = "button5";
-            button5.Size = new Size(107, 32);
-            button5.TabIndex = 19;
-            button5.Text = "Fiction";
+            button5.Size = new Size(233, 57);
+            button5.TabIndex = 28;
+            button5.Text = "Suspense";
             button5.UseVisualStyleBackColor = false;
             // 
             // CategoriesForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1284, 626);
+            ClientSize = new Size(1603, 719);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button1);
-            Controls.Add(button2);
+            Controls.Add(button6);
             Controls.Add(label1);
             Controls.Add(pictureBox5);
             Controls.Add(pictureBox4);
@@ -192,7 +202,6 @@
             Controls.Add(pictureBox1);
             Name = "CategoriesForm";
             Text = "Categories";
-            Load += CategoriesForm_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
@@ -210,7 +219,7 @@
         private PictureBox pictureBox4;
         private PictureBox pictureBox5;
         private Label label1;
-        private Button button2;
+        private Button button6;
         private Button button1;
         private Button button3;
         private Button button4;
