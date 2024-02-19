1c65df8e305d3ae37b3dfb1f45ab34a117dab9c3
Ahmed abdelgaber Mortada <ahmedabdelgaber74@gmail.com>
2024-02-19 20:45:06 +0200

    Admin Order

--- BookStore.Admin/Forms/OrdersForm.Designer.cs
+++ BookStore.Admin/Forms/OrdersForm.Designer.cs
@@ -20,41 +20,194 @@
       base.Dispose(disposing);
     }
 
-    #region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-      dataGridView1 = new DataGridView();
-      ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-      SuspendLayout();
-      // 
-      // dataGridView1
-      // 
-      dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-      dataGridView1.Location = new Point(378, 71);
-      dataGridView1.Name = "dataGridView1";
-      dataGridView1.RowHeadersWidth = 62;
-      dataGridView1.Size = new Size(382, 249);
-      dataGridView1.TabIndex = 0;
-      // 
-      // OrdersForm
-      // 
-      AutoScaleDimensions = new SizeF(10F, 25F);
-      AutoScaleMode = AutoScaleMode.Font;
-      ClientSize = new Size(800, 450);
-      Controls.Add(dataGridView1);
-      Name = "OrdersForm";
-      Text = "Orders";
-      ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-      ResumeLayout(false);
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            label1 = new Label();
+            label3 = new Label();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridView1.BackgroundColor = SystemColors.GradientActiveCaption;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(292, 33);
+            dataGridView1.Margin = new Padding(2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(481, 202);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.OrangeRed;
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(216, 372);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 34);
+            button1.TabIndex = 1;
+            button1.Text = "View Order";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += ViewAll_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.OrangeRed;
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(608, 372);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 34);
+            button2.TabIndex = 2;
+            button2.Text = "Cancle Order";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += Cancle_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(33, 21);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "ID";
+            textBox1.Size = new Size(157, 31);
+            textBox1.TabIndex = 3;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(33, 80);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Status";
+            textBox2.Size = new Size(157, 34);
+            textBox2.TabIndex = 4;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(33, 140);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Adress";
+            textBox3.Size = new Size(157, 35);
+            textBox3.TabIndex = 5;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(33, 209);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Price";
+            textBox4.Size = new Size(157, 34);
+            textBox4.TabIndex = 6;
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dateTimePicker1.Location = new Point(158, 318);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 7;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dateTimePicker2.Location = new Point(591, 318);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 21);
+            dateTimePicker2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Maroon;
+            label1.Location = new Point(12, 318);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 17);
+            label1.TabIndex = 9;
+            label1.Text = " Ordered  Date Time";
+            label1.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Maroon;
+            label3.Location = new Point(465, 322);
+            label3.Name = "label3";
+            label3.Size = new Size(98, 17);
+            label3.TabIndex = 11;
+            label3.Text = "Arrived Order";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox4);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(221, 276);
+            panel1.TabIndex = 12;
+            // 
+            // OrdersForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Linen;
+            ClientSize = new Size(803, 418);
+            Controls.Add(panel1);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Margin = new Padding(2);
+            Name = "OrdersForm";
+            Text = "Orders";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label label1;
+        private Label label3;
+        private Panel panel1;
     }
-
-    #endregion
-
-    private DataGridView dataGridView1;
-  }
 }
